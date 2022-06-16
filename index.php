0639d4203389b20a72313790a5e0e826ce5673e3
Coelho_yaq <86680741+coelho03@users.noreply.github.com>
2022-06-16 19:55:38 -0300

    update and finish index.php (login)
    Estilizado, completo e cheiroso

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<title>Login</title>
 </head>
 <body>
-		<div id="painel">
-			<form method="POST" action="pag1.php">
-				<h1>Insira seu login...</h1>
+	<div id="site">
+			<form method="POST" action="pag1.php" id="form">
+				<h1>Login</h1>
 				<p>Username:</p>
-					<input type="text" name="user">
+					<input type="text" name="user" id="campo_login" required>
 				<p>Senha:</p>
-					<input type="text" name="senha">
+					<input type="password" name="senha" id="campo_login" required><br>
 				<button id="enviar" type="submit">Enviar</button>
 			</form>
-		</div>
+	</div>
 </body>
 </html>
