4fdc767151264334791111ec2f52db25a1dcc587
Coelho_yaq <86680741+coelho03@users.noreply.github.com>
2022-06-16 18:19:04 -0300

    update and finish index.html
    Just add what is it really important and finish the details

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<?php session_start(); ?>
+<?php session_start();?>
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="styles.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<title>Login</title>
 </head>
 <body>
 		<div id="painel">
-			<form method="post" action="pag1.php">
+			<form method="POST" action="pag1.php">
 				<h1>Insira seu login...</h1>
 				<p>Username:</p>
-					<input type="text" name="username" id="campo_login">
+					<input type="text" name="user">
 				<p>Senha:</p>
-					<input type="text" name="senha" id="campo_login">
+					<input type="text" name="senha">
 				<button id="enviar" type="submit">Enviar</button>
 			</form>
 		</div>
