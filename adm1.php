185949825bf8531871df727ccb20eb6145485ad3
Coelho_yaq <86680741+coelho03@users.noreply.github.com>
2022-06-16 22:50:35 -0300

    upload "admin.php"
    access just for "premium" logins

---
+++ adm1.php
@@ -0,0 +1,34 @@
+<?php 
+	session_start();
+	$_SESSION['user'] = $_POST['user'];
+	$_SESSION['senha'] = $_POST['senha'];
+
+	if (!isset($_SESSION['user']) or !isset($_SESSION['senha'])) {
+		header('location: index.php');
+	}elseif ($_SESSION['user'] == 'root' and $_SESSION['senha'] == 'usbw') {
+		?>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title>ADM</title>
+		</head>
+		<style type="text/css">
+			*{
+				margin: 0;
+				padding: 0;
+			}
+			html, body{
+				height: 100%;
+			}
+		</style>
+		<body>
+			<center>
+				<img src="https://i.pinimg.com/564x/b1/92/c7/b192c70d750d0700becf81b1ccc9e2a6.jpg">
+			</center>
+		</body>
+		</html>
+		<?php
+	}
+?>
