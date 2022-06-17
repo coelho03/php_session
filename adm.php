21028606220c91c1a3e39b637d89196133b175d7
Coelho_yaq <86680741+coelho03@users.noreply.github.com>
2022-06-17 14:54:35 -0300

    update and complete adm.php
    add logout button

--- adm.php
+++ adm.php
@@ -1,7 +1,5 @@
 <?php 
 	session_start();
-	$_SESSION['user'] = $_POST['user'];
-	$_SESSION['senha'] = $_POST['senha'];
 
 	if (!isset($_SESSION['user']) or !isset($_SESSION['senha'])) {
 		header('location: index.php');
@@ -22,10 +20,30 @@
 			html, body{
 				height: 100%;
 			}
+			#logout{	
+				height: 40px;
+				width: 30%;
+
+				font-size: 110%;
+				text-decoration: none;
+				color: #000;
+
+				background-color: transparent;
+				border-radius: 14px;
+				border-style: solid;
+				border-color: #000;
+				border-width: 2px;
+
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+				align-items: center;
+			}
 		</style>
 		<body>
 			<center>
 				<img src="https://i.pinimg.com/564x/b1/92/c7/b192c70d750d0700becf81b1ccc9e2a6.jpg">
+				<a type="button" href="logout.php" id="logout">LOGOUT</a>
 			</center>
 		</body>
 		</html>
