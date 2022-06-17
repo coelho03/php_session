3a32dc2b95edb7d76299034145fd8b833a2cbb31
Coelho_yaq <86680741+coelho03@users.noreply.github.com>
2022-06-17 14:50:08 -0300

    update and complete user.php
    add "logout_button"

--- user.php
+++ user.php
@@ -20,10 +20,34 @@
 			html, body{
 				height: 100%;
 			}
+			img{
+				height: 200px;
+				width: 200px;
+			}
+			#logout{
+				height: 40px;
+				width: 30%;
+
+				font-size: 110%;
+				text-decoration: none;
+				color: #000;
+
+				background-color: transparent;
+				border-radius: 14px;
+				border-style: solid;
+				border-color: #000;
+				border-width: 2px;
+
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+				align-items: center;
+			}
 		</style>
 		<body>
 			<center>
 				<img src="https://i.pinimg.com/564x/b1/92/c7/b192c70d750d0700becf81b1ccc9e2a6.jpg">
+				<a type="button" href="logout.php" id="logout">LOGOUT</a>
 			</center>
 		</body>
 		</html>
@@ -37,10 +61,43 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<title>USER</title>
 		</head>
+		<style type="text/css">
+			*{
+				margin: 0;
+				padding: 0;
+			}
+			html, body{
+				height: 100%;
+			}
+			img{
+				height: 200px;
+				width: 200px;
+			}
+			#logout{
+				height: 40px;
+				width: 30%;
+
+				font-size: 110%;
+				text-decoration: none;
+				color: #000;
+
+				background-color: transparent;
+				border-radius: 14px;
+				border-style: solid;
+				border-color: #000;
+				border-width: 2px;
+
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+				align-items: center;
+			}
+		</style>
 		<body>
 			<center>
 				<h1> USER </h1>
-				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/2048px-User_font_awesome.svg.png" height="800px" width="800px">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/2048px-User_font_awesome.svg.png">
+				<a type="button" href="logout.php" id="logout">LOGOUT</a>
 			</center>
 		</body>
 		</html>
